86d25a413be859609b37072ad59953b786f44689
sogasg <sogasg@gmail.com>
2021-06-21 22:28:41 +0200

    Add Badger Sett Vault interface

---
+++ contracts/release/interfaces/IBadgerSettVault.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+    This file is part of the Enzyme Protocol.
+
+    (c) Enzyme Council <council@enzyme.finance>
+
+    For the full license information, please view the LICENSE
+    file that was distributed with this source code.
+*/
+
+pragma solidity 0.6.12;
+
+/// @title IBadgerSettVault Interface
+/// @author Asgeir
+/// @notice Minimal interface for our interactions with Badger Sett Vault contracts
+interface IBadgerSettVault {
+    function deposit(uint256) external;
+
+    function depositAll() external;
+
+    function getPricePerFullShare() external view returns (uint256);
+
+    function token() external view returns (address);
+
+    function withdraw(uint256) external;
+
+    function withdrawAll() external;
+}
