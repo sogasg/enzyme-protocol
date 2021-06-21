d2aca2d7c0df93f40ac3dcb6cc64b7c2ef749eba
sogasg <sogasg@gmail.com>
2021-06-21 23:10:05 +0200

    Add Badger Sett Vault actions

---
+++ contracts/release/extensions/integration-manager/integrations/utils/actions/BadgerSettVaultActionsMixin.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+    This file is part of the Enzyme Protocol.
+
+    (c) Enzyme Council <council@enzyme.finance>
+
+    For the full license information, please view the LICENSE
+    file that was distributed with this source code.
+*/
+
+pragma solidity 0.6.12;
+
+import "../../../../../interfaces/IBadgerSettVault.sol";
+import "../../../../../utils/AssetHelpers.sol";
+
+/// @title BadgerSettVaultActionsMixin Contract
+/// @author Asgeir
+/// @notice Mixin contract for interacting with Badger Sett Vaults
+abstract contract BadgerSettVaultActionsMixin is AssetHelpers {
+    /// @dev Helper to lend underlying for Badger Sett Vault shares
+    function __badgerSettVaultLend(
+        address _badgerSettVault,
+        address _underlying,
+        uint256 _underlyingAmount
+    ) internal {
+        __approveAssetMaxAsNeeded(_underlying, _badgerSettVault, _underlyingAmount);
+        IBadgerSettVault(_badgerSettVault).deposit(_underlyingAmount);
+    }
+
+    /// @dev Helper to redeem Badger Sett Vault shares for underlying
+    function __badgerSettVaultRedeem(
+        address _badgerSettVault,
+        uint256 _yVaultSharesAmount
+    ) internal {
+        // TODO: What about frontrunning? This could be a big order. Is it a problem?
+        IBadgerSettVault(_badgerSettVault).withdraw(_yVaultSharesAmount);
+    }
+}
